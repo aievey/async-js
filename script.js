b229417ffe1567fa58a8661ab9639f024bfc343f
aievey <amitabh283verma@gmail.com>
2026-01-05 23:46:04 +0530

    Ok

--- script.js
+++ script.js
@@ -10,3 +10,25 @@ const countriesContainer = document.querySelector('.countries');
 // https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
 
 ///////////////////////////////////////
+const request = new XMLHttpRequest();
+request.open('GET', 'https://restcountries.com/v3/name/india');
+request.send();
+
+request.addEventListener('load', function () {
+  const [, data] = JSON.parse(this.responseText);
+  console.log(data);
+  const html = `
+  <article class="country">
+            <img class="country__img" src="${data.flags[1]}" />
+            <div class="country__data">
+              <h3 class="country__name">${data.name.common}</h3>
+              <h4 class="country__region">${data.region}</h4>
+              <p class="country__row"><span>👫</span>${data.population}</p>
+              <p class="country__row"><span>🗣️</span>${data.languages.hin}</p>
+              <p class="country__row"><span>💰</span>${
+                data.currencies.INR.symbol + ' ' + data.currencies.INR.name
+              }</p>
+            </div>
+          </article>`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+});
