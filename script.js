e74bf073b52ff1509c5c2eadb33c5e66ba9e860d
aievey <amitabh283verma@gmail.com>
2026-01-06 11:10:11 +0530

    updated countries API to version 2

--- script.js
+++ script.js
@@ -1,34 +1,41 @@
 'use strict';
+const getCountryData = function (country) {
+  const btn = document.querySelector('.btn-country');
+  const countriesContainer = document.querySelector('.countries');
 
-const btn = document.querySelector('.btn-country');
-const countriesContainer = document.querySelector('.countries');
+  // NEW COUNTRIES API URL (use instead of the URL shown in videos):
+  // https://restcountries.com/v2/name/portugal
 
-// NEW COUNTRIES API URL (use instead of the URL shown in videos):
-// https://restcountries.com/v2/name/portugal
+  // NEW REVERSE GEOCODING API URL (use instead of the URL shown in videos):
+  // https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
 
-// NEW REVERSE GEOCODING API URL (use instead of the URL shown in videos):
-// https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
+  ///////////////////////////////////////
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
 
-///////////////////////////////////////
-const request = new XMLHttpRequest();
-request.open('GET', 'https://restcountries.com/v3/name/india');
-request.send();
-
-request.addEventListener('load', function () {
-  const [, data] = JSON.parse(this.responseText);
-  console.log(data);
-  const html = `
+  request.addEventListener('load', function () {
+    const [, data] = JSON.parse(this.responseText);
+    console.log(data);
+    const html = `
   <article class="country">
-            <img class="country__img" src="${data.flags[1]}" />
+            <img class="country__img" src="${data.flag}" />
             <div class="country__data">
-              <h3 class="country__name">${data.name.common}</h3>
+              <h3 class="country__name">${data.name}</h3>
               <h4 class="country__region">${data.region}</h4>
               <p class="country__row"><span>👫</span>${data.population}</p>
-              <p class="country__row"><span>🗣️</span>${data.languages.hin}</p>
-              <p class="country__row"><span>💰</span>${
-                data.currencies.INR.symbol + ' ' + data.currencies.INR.name
-              }</p>
+              <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+              <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
             </div>
           </article>`;
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-});
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('china');
+getCountryData('india');
+getCountryData('uk');
+getCountryData('german');
+getCountryData('french');
+getCountryData('america');
